ccd59f4261d276b014de2bfc52de094345cc49b9
juliantellez <4896851+juliantellez@users.noreply.github.com>
2025-03-23 23:14:04 +0000

    products(frontend): use rsbuild and tsconfig

---
+++ products/maistro-websites/frontend/rsbuild.config.js
@@ -0,0 +1,7 @@
+const { createBaseConfig } = require('@maistro/rsbuild-config');
+
+module.exports = createBaseConfig({
+    port: 3000,
+    title: 'Maistro Websites',
+    entry: './src/index.tsx',
+});
