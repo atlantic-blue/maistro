901b9b14ed15e9b35ba1bb61f6456eb267939762
juliantellez <4896851+juliantellez@users.noreply.github.com>
2025-03-23 23:30:10 +0000

    packages(frontend): add eslint

---
+++ products/maistro-socials/frontend/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['@maistro/eslint-config'],
+    parserOptions: {
+      project: './tsconfig.json'
+    },
+    rules: {
+      // Add any project-specific overrides here
+    }
+  };
