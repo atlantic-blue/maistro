47eb98f9075eb846a14d4968a05dec9351bec7f3
juliantellez <4896851+juliantellez@users.noreply.github.com>
2025-04-11 22:16:21 +0100

    format

--- products/maistro-design/frontend/src/App.tsx
+++ products/maistro-design/frontend/src/App.tsx
@@ -3,11 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './styles/global.css';
 
 const App = () => {
-  return (
-    <div className="min-h-screen bg-gray-100 p-8">
-      Design
-    </div>
-  );
+  return <div className="min-h-screen bg-gray-100 p-8">Design</div>;
 };
 
 // Mount the React application
