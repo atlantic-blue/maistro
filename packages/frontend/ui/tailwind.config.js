829416768777cb040cd8241c375dfca3230b0c8a
juliantellez <4896851+juliantellez@users.noreply.github.com>
2025-04-11 19:34:54 +0100

    packages-frontend-ui-colours-typography

--- packages/frontend/ui/tailwind.config.js
+++ packages/frontend/ui/tailwind.config.js
@@ -1,8 +1,17 @@
+const { maistroColours } = require("./src/colours");
+const { typography } = require('./src/styles/typography');
+
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-      extend: {},
+        extend: {
+            colors: maistroColours,
+            fontFamily: typography.fontFamily,
+            fontSize: typography.fontSize,
+            fontWeight: typography.fontWeight,
+            lineHeight: typography.lineHeight,
+            letterSpacing: typography.letterSpacing,
+        },
     },
     plugins: [],
-  };
-  
+};
