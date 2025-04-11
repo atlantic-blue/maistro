affab23a8068e2cdf9d24505c94eb449dca6ea58
Julian Tellez <4896851+juliantellez@users.noreply.github.com>
2025-04-11 23:41:42 +0100

    products(design): storybook (#5)

--- products/maistro-design/frontend/eslint.config.mjs
+++ products/maistro-design/frontend/eslint.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from "eslint/config";
+import storybook from 'eslint-plugin-storybook'
 import maistroEslintConfig from "@maistro/eslint-config";
 
-export default defineConfig(maistroEslintConfig);
+export default defineConfig([
+    ...maistroEslintConfig,
+    ...storybook.configs['flat/recommended'],
+    {
+        rules: {
+            'no-redeclare': 'off',
+        },
+    }
+]);
