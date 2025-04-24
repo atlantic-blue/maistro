b71a2232646b4866669aa107e7fa7d5502adad20
juliantellez <4896851+juliantellez@users.noreply.github.com>
2025-04-24 22:31:43 +0100

    products: website

---
+++ products/maistro-websites/frontend/src/Routes/appRoutes.tsx
@@ -0,0 +1,50 @@
+export enum Routes {
+    HOME = '/*',
+    SOCIALS = '/socials',
+    TEMPLATES = '/templates',
+    ANALYTICS = '/analytics',
+    SETTINGS = '/settings',
+  
+    AUTHZ_LOGIN = "/login",
+    AUTHZ_LOGOUT = "/logout",
+    AUTH_CALLBACK = "/callback",
+
+    USER_SETTINGS = "/user/settings"
+  }
+  
+  class AppRoutes {
+    getHomeRoute() {
+      return `/`
+    }
+  
+    getLoginRoute() {
+      return `${Routes.AUTHZ_LOGIN}`
+    }
+  
+    getLogoutRoute() {
+        return `${Routes.AUTHZ_LOGOUT}`
+    }
+  
+    getWebsites() {
+      return `${Routes.SOCIALS}`
+    }
+
+    getTemplates() {
+      return `${Routes.TEMPLATES}`
+    }
+
+    getAnalytics() {
+      return `${Routes.ANALYTICS}`
+    }
+
+    getSettings() {
+      return `${Routes.SETTINGS}`
+    }
+
+    getUserSettings() {
+      return `${Routes.USER_SETTINGS}`
+    }
+  }
+  
+export const appRoutes = new AppRoutes()
+  
